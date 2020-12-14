4acb865751375ae8a105b1376e0eebe51e9fd739
hasbiak <hasbialwi70@gmail.com>
2020-12-14 12:41:34 +0700

    Mysql Finish

--- src/routesNavigation.js
+++ src/routesNavigation.js
@@ -2,10 +2,8 @@ const router = require('express').Router()
 
 const product = require('./routes/r_product')
 const category = require('./routes/r_category')
-const coupon = require('./routes/r_coupon')
 
 router.use('/product', product)
 router.use('/category', category)
-router.use('/coupon', coupon)
 
 module.exports = router
